66a87e26ee1332e02ae0dc6856e116aa3563c220
KyuHyeonYang <95363270@naver.com>
2024-08-13 17:07:53 +0900

    0813커밋,

--- src/main/resources/static/js/communitymain.js
+++ src/main/resources/static/js/communitymain.js
@@ -1,9 +1,31 @@
 /**
- * 커뮤니티메인 사이드바 클릭하면 검은색상,볼드로변경
+ * 
  */
  
+
+
+
+$(document).ready(function(){
  
-$(document).ready(function() {
+    // 맨위로 버튼 
+    $(window).on('scroll', function () {
+        toggleScrollButton();
+    });
+
+    function toggleScrollButton() {
+        let $toTopBtn = $("#to_top_Btn");
+        if ($(window).scrollTop() > 800) {
+            $toTopBtn.css("display", "flex");
+        } else {
+            $toTopBtn.css("display", "none");
+        }
+    }
+
+    $("#to_top_Btn").on("click", function () {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+
+    // 사이드바 링크 클릭 시 스타일 변경
     const navLinks = $('.nav-link');
 
     navLinks.on('click', function() {
@@ -27,15 +49,12 @@ $(document).ready(function() {
                     console.log(" 프로필 표시");
                     $('#after-login').show();
 
-                    //프로필사진설정
-					let profileImage =  response.profileImageUrl ||
- 					'/images/default-profile.png';  // 기본 프로필 이미지 경로 설정해야댐
+                    // 프로필 사진 설정
+                    let profileImage = response.profileImageUrl || '/image/default-profile.png';  // 기본 프로필 이미지 경로 설정
                     $('#profile-img').attr('src', profileImage);  // 프로필 이미지 업데이트
                     
-                    
-                    console.log(response.user_nickname);
                     console.log(response);
-                    //프로필정보가ㅈ고오기
+                    // 프로필 정보 가져오기
                     $('#nickname').text(response.user_nickname);
                     $('#timestamp').text(response.user_timestamp);
                     $('#level').text(response.user_rate);
@@ -45,18 +64,18 @@ $(document).ready(function() {
                     $('#after-login').hide();
                 }
             },
-       error: function(xhr, status, error) {
-            if (xhr.status === 401) {
-                // 401 Unauthorized인 경우: 로그인되지 않은 상태
-                $('#before-login').show();
-                $('#after-login').hide();
-            } else {
-                alert("로그인 상태 확인 실패: " + status + " - " + error);
+            error: function(xhr, status, error) {
+                if (xhr.status === 401) {
+                    // 401 Unauthorized인 경우: 로그인되지 않은 상태
+                    $('#before-login').show();
+                    $('#after-login').hide();
+                } else {
+                    alert("로그인 상태 확인 실패: " + status + " - " + error);
+                }
             }
-        }
-    });
-}
-//로그인안한상태에서 이함수는오류처리되고있음..
-// 페이지 로드 시 로그인 상태 확인
-checkLoginStatus();
+        });
+    }
+
+    // 페이지 로드 시 로그인 상태 확인
+    checkLoginStatus();
 });
