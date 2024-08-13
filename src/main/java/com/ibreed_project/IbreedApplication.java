5ab20a97c93edbeb077449129c17808df2af41f4
yeeun21 <jye129@gmail.com>
2024-08-13 17:17:49 +0900

    메인화면 로그인 적용 및 다이어리 디비 연동

--- src/main/java/com/ibreed_project/IbreedApplication.java
+++ src/main/java/com/ibreed_project/IbreedApplication.java
@@ -4,10 +4,20 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.ibreed_project"})
 @MapperScan(basePackages = {"com.ibreed_project"})
+/*
+ * @PropertySources({
+ * 
+ * @PropertySource(value="file:C:\\ibreed_project\\configure.properties",
+ * ignoreResourceNotFound=true) })
+ */
+
+
 public class IbreedApplication {
 
 	public static void main(String[] args) {
