59d716e721db37068039a8e61a57c4665f86f358
yrotciv22 <glep6500@gmail.com>
2024-08-20 09:18:53 +0900

    앨범 작업 완료 > 앨범상세 진행 예정

---
+++ src/main/resources/static/js/albumCheck.js
@@ -0,0 +1,37 @@
+/**
+ * albumCheck.js
+ */
+ 
+  $(document).ready(function() {
+ 	$('#album_Add').on('submit', function() {
+ 		event.preventDefault();
+ 		
+ 		let album_name = $('#update_text').val();
+ 		
+ 		if(album_name == "") {
+ 			alert("앨범명을 입력하세요");
+ 			return false;
+ 		} else {
+		$.ajax({
+	 			type:"post",
+	 			url:"/album/addAlbum", 
+	 			data : {"album_name": album_name},
+	 			dataType:'text',
+	 			success:function(result) {
+	 				if(result == "not_available") {
+	 					alert("사용할 수 없는 앨범명입니다");
+	 				} else {
+	 					// 어떻게 해야?
+	 					return "redirect:/mydiary/photos";
+	 				}
+	 			},
+	 			error:function() {
+	 				console.log("실패");
+	 			}
+	 		});
+ 		
+ 		} // else 끝
+ 	
+ 	}); // click 끝
+ 
+ });
